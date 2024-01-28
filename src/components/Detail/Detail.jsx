56a82770fe59e0b404252d1f8de7059d48cb2ae7
Dustin Hagstrom <dustnhgstrm@gmail.com>
2024-01-27 18:02:41 -0600

    wrote the rest of the details into the Detail component. wrote button with click function to route the user back to the homepage.

--- src/components/Detail/Detail.jsx
+++ src/components/Detail/Detail.jsx
@@ -1,13 +1,19 @@
 import { useSelector } from "react-redux";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 
 export default function Detail({ movieDetail }) {
     console.log("movie from state:", movieDetail);
+
+    const history = useHistory();
 
     return (
         <>
             <div>
                 <h3>{movieDetail.title}</h3>
                 <img src={movieDetail.poster} alt={movieDetail.title} />
+                <p>{movieDetail.description}</p>
+                <p>{movieDetail.genre}</p>
+                <button onClick={() => history.push("/")}>Back</button>
             </div>
         </>
     );
